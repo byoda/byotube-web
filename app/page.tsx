6f080a21d073fee9824854b3ae96d11b43139db0
Steven Hessing <stevenhessing@live.com>
2023-07-11 19:46:27 -0700

    constant FlatList with reactive-native-web works

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,55 @@
-// 'use client'
+'use client'
 
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 
-import { Metadata } from 'next'
+import { SafeAreaView, View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native-web';
 
-export const metadata: Metadata = {
-  title: 'Bring Your Own Tube'
-}
+// import { Metadata } from 'next'
+
+
+// const columns = [
+//    { key: 'id', name: 'ID' },
+//    { key: 'title', name: 'Title' }
+// ];
+
+const DATA = [
+    {
+        'id': '1',
+        'title': 'First Item',
+    },
+    {
+        'id': '2',
+        'title': 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+]
+
+const Item = ({item}) => (
+  <View>
+      <Text>{item.title}</Text>
+  </View>
+);
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+    const renderItem = ({ item }) => {
+        <View>
+            <TouchableOpacity key={item.id}>
+                <Text>{item.title}</Text>
+            </TouchableOpacity>
+        </View>
+    }
+
+    return (
+        <SafeAreaView>
+            <FlatList
+                data={DATA}
+                numColumns={2}
+                renderItem={({item}) => <Item item={item} />}
+                keyExtractor={item => item.id}
+            />
+        </SafeAreaView>
+    );
+};
