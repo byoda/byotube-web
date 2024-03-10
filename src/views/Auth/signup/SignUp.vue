cf51b5b61f34a37239145a5b35211c6225305a10
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2024-03-10 08:14:01 +0500

    feat: tooltips

--- src/views/Auth/signup/SignUp.vue
+++ src/views/Auth/signup/SignUp.vue
@@ -1,42 +1,75 @@
 <template>
   <v-container fluid class="fill-height">
     <v-row>
-      <v-col cols="12" xs="12" sm="8" md="7" lg="7" class="ma-auto shadow-smooth" >
+      <v-col cols="12" xs="12" sm="8" md="8" lg="7" class="ma-auto shadow-smooth">
         <v-card class="rounded-lg pa-5" outlined :loading="loading">
           <div class="text-center pt-7">
-              <img src="@/assets/byotube-logo.png" :width="300" alt="" srcset="">
-            </div>
+            <img src="@/assets/byotube-logo.png" :width="300" alt="" srcset="">
+          </div>
           <v-row class="justify-space-between px-4 pt-6 pb-12">
             <v-col cols="12">
-              <v-data-table-server :headers="headers" :items="rows">
-                <template #item.anonymous="{value}">
+              <v-data-table :headers="headers" :items="rows">
+                <template #header.liteFunded>
+                  <p>
+                    Lite - funded(*)
+                    <v-tooltip activator="parent" :text="liteFundedTooltip" location="top">
+                    </v-tooltip>
+                  </p>
+                </template>
+                <template #item.byotube="{ value }">
+                  <div class="d-flex ga-2">
+                    <p>
+                      {{ value }}
+                    </p>
+                    <v-tooltip activator="parent" :text="getFeatureTooltip(value)" location="top">
+                      <template v-slot:activator="{ props }">
+                        <v-icon v-bind="props">
+                          mdi-information-slab-circle-outline
+                        </v-icon>
+                      </template>
+                    </v-tooltip>
+                  </div>
+                </template>
+                <template #item.anonymous="{ value }">
                   <v-chip :color="getIconColor(value)" rounded="circle" class="circle-chip" :pill="true">
                     <v-icon>
-                      mdi-{{getIcon(value)}}
+                      mdi-{{ getIcon(value) }}
                     </v-icon>
                     {{ getValue(value) }}
                   </v-chip>
                 </template>
-                <template #item.lite="{value}">
+                <template #item.lite="{ value, item }">
                   <v-chip :color="getIconColor(value)" class="circle-chip">
+                    <v-tooltip v-if="value.includes('*')" activator="parent" :text="getContentTooltip(value, item)" location="top">
+                    </v-tooltip>
                     <v-icon>
-                      mdi-{{getIcon(value)}}
+                      mdi-{{ getIcon(value) }}
                     </v-icon>
                     {{ getValue(value) }}
                   </v-chip>
                 </template>
-                <template #item.plain="{value}">
+                <template #item.liteFunded="{ value }">
                   <v-chip :color="getIconColor(value)" class="circle-chip">
                     <v-icon>
-                      mdi-{{getIcon(value)}}
+                      mdi-{{ getIcon(value) }}
                     </v-icon>
                     {{ getValue(value) }}
                   </v-chip>
                 </template>
-                <template #item.creator="{value}">
+                <template #item.plain="{ value, item }">
+                  <v-chip :color="getIconColor(value)" class="circle-chip">
+                    <v-tooltip v-if="value.includes('*')" activator="parent" :text="getContentTooltip(value, item)" location="top">
+                    </v-tooltip>
+                    <v-icon>
+                      mdi-{{ getIcon(value) }}
+                    </v-icon>
+                    {{ getValue(value) }}
+                  </v-chip>
+                </template>
+                <template #item.creator="{ value }">
                   <v-chip :color="getIconColor(value)" class="circle-chip">
                     <v-icon>
-                      mdi-{{getIcon(value)}}
+                      mdi-{{ getIcon(value) }}
                     </v-icon>
                     {{ getValue(value) }}
                   </v-chip>
@@ -44,7 +77,7 @@
                 <template #bottom>
                   <div></div>
                 </template>
-              </v-data-table-server>
+              </v-data-table>
             </v-col>
             <v-col cols="12">
               <v-card-subtitle class="mb-5">Create your VueTube account</v-card-subtitle>
@@ -52,21 +85,29 @@
                 <form @submit.prevent="handleSubmit(signUp)" @reset.prevent="reset">
                   <v-row>
                     <v-col cols="12" md="6" class="pb-0">
-                      <BaseTextfield v-model="email" :error-messages="errors" label="Email" color="primary" name="Email" class="mb-3" variant="outlined"/>
+                      <BaseTextfield v-model="email" :error-messages="errors" label="Email" color="primary" name="Email"
+                        class="mb-3" variant="outlined" />
                     </v-col>
                     <v-col cols="12" md="6" class="pb-0">
-                      <BaseTextfield v-model="channelName" label="Channel Name" name="Channel Name" color="primary" variant="outlined" />
+                      <BaseTextfield v-model="channelName" label="Channel Name" name="Channel Name" color="primary"
+                        variant="outlined" />
                     </v-col>
                     <v-col cols="12" md="6" class="py-0">
-                      <BaseTextfield v-model="password" type="password" name="Password" label="Password" color="primary" variant="outlined" />
+                      <BaseTextfield v-model="password" type="password" name="Password" label="Password" color="primary"
+                        variant="outlined" />
                     </v-col>
                     <v-col cols="12" md="6" class="py-0">
-                      <BaseTextfield type="password" v-model="confirmPassword" name="Confirm Password" color="primary" label="Confirm Password" variant="outlined" /> 
+                      <BaseTextfield type="password" v-model="confirmPassword" name="Confirm Password" color="primary"
+                        label="Confirm Password" variant="outlined" />
                     </v-col>
                   </v-row>
                   <div class="mt-6">
-                    <p class="pl-0 text-capitalize mb-2" >Already have and account? <span class="text-primary cursor-pointer" @click="$router.push({name:'SignIn'})">Signin</span>  here</p>
-                    <BaseBtn block type="submit" class="bg-primary elevation-0 white-text"  :loading="loading" depressed>Sign up</BaseBtn>
+                    <p class="pl-0 text-capitalize mb-2">Already have and account? <span
+                        class="text-primary cursor-pointer" @click="$router.push({ name: 'SignIn' })">Signin</span> here
+                    </p>
+                    <BaseBtn block type="submit" class="bg-primary elevation-0 white-text" :loading="loading" depressed>
+                      Sign up
+                    </BaseBtn>
                   </div>
                 </form>
               </v-card-text>
@@ -84,7 +125,7 @@ import { useSignup } from './useSignup';
 import { BaseBtn, BaseTextfield } from '@/components/base';
 
 
-const { getIcon, getIconColor, getValue } = useSignup()
+const { liteFundedTooltip, rows, getIcon, getIconColor, getValue, getFeatureTooltip, getContentTooltip } = useSignup()
 
 const email = ref('')
 const channelName = ref('')
@@ -98,152 +139,55 @@ const headers = [
     align: 'start',
     sortable: false,
     key: 'byotube',
+    width: '180'
   },
-  { title: 'Anonymous', key: 'anonymous', align: 'center' },
+  { title: 'Anonymous', key: 'anonymous', align: 'center', width:'50' },
   { title: 'Lite', key: 'lite', align: 'center' },
+  { title: 'Lite - funded(*)', key: 'liteFunded', align: 'center' },
   { title: 'BYODA-plain (g)', key: 'plain', align: 'center' },
   { title: 'BYODA-(Top) Creator (g)', key: 'creator', align: 'center' },
 ]
 
-const rows = [
-  {
-    byotube:'History',
-    anonymous: 'No',
-    lite:'Yes',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Algorithm selection',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes(1)',
-    creator:'Yes(1)',
-  },
-  {
-    byotube:'Likes',
-    anonymous: 'No',
-    lite:'Yes(2)',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Follow',
-    anonymous: 'No',
-    lite:'Yes(2)',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Bookmarks',
-    anonymous: 'No',
-    lite:'Yes',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Comments',
-    anonymous: 'No',
-    lite:'Yes(1,2)',
-    plain:'Yes(1)',
-    creator:'Yes(1)',
-  },
-  {
-    byotube:'Nickname',
-    anonymous: 'No',
-    lite:'Yes(2)',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Lists',
-    anonymous: 'No',
-    lite:'Yes(1)',
-    plain:'Yes(1)',
-    creator:'Yes(1)',
-  },
-  {
-    byotube:'Subscribe Creator',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Subscribe Ultra',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Subscribe Ultra',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Tip',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes',
-    creator:'Yes',
-  },
-  {
-    byotube:'Video upload',
-    anonymous: 'No',
-    lite:'No',
-    plain:'No',
-    creator:'Yes(1)',
-  },
-  {
-    byotube:'Youtube Import',
-    anonymous: 'No',
-    lite:'No',
-    plain:'Yes(3)',
-    creator:'Yes',
-  },
-]
+
 
 
 const signUp = async () => {
-      loading.value = true
+  loading.value = true
 
-      // const data = await this.$store
-      //   .dispatch('signUp', {
-      //     email: email.value,
-      //     channelName: channelName.value,
-      //     password: password.value
-      //   })
-      //   .catch((err) => {
-      //     loading.value = false
-      //     const errors = err.response.data.error
+  // const data = await this.$store
+  //   .dispatch('signUp', {
+  //     email: email.value,
+  //     channelName: channelName.value,
+  //     password: password.value
+  //   })
+  //   .catch((err) => {
+  //     loading.value = false
+  //     const errors = err.response.data.error
 
-      //     this.$refs.form.setErrors({
-      //       'Email': errors.find((error) => {
-      //         return error.field === 'email'
-      //       })
-      //         ? ['This email is already taken']
-      //         : null,
-      //       'Channel Name': errors.find((error) => {
-      //         return error.field === 'channelName'
-      //       })
-      //         ? ['This channel name is already taken']
-      //         : null
-      //     })
-      //   })
+  //     this.$refs.form.setErrors({
+  //       'Email': errors.find((error) => {
+  //         return error.field === 'email'
+  //       })
+  //         ? ['This email is already taken']
+  //         : null,
+  //       'Channel Name': errors.find((error) => {
+  //         return error.field === 'channelName'
+  //       })
+  //         ? ['This channel name is already taken']
+  //         : null
+  //     })
+  //   })
 
-      if (!data) return
+  if (!data) return
 
-      if (!user) return
-      loading.value = false
-      this.$router.push({ name: 'Home' })
-    }
+  if (!user) return
+  loading.value = false
+  this.$router.push({ name: 'Home' })
+}
 </script>
 
 <style scoped>
-.circle-chip{
+.circle-chip {
   padding: 6px !important;
 }
 </style>
--- src/views/Auth/signup/useSignup.js
+++ src/views/Auth/signup/useSignup.js
@@ -13,6 +13,165 @@ export const useSignup = () => {
   const { loader, showLoader, hideLoader } = useLoader();
   const { signIn: signinReq } = useAuthService();
 
+  const rows = [
+    {
+      byotube: "Choose Nickname",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Choose Handle",
+      anonymous: "No",
+      lite: "Yes",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Follow Creator",
+      anonymous: "No",
+      lite: "Yes(*)",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Subscribe to Creator",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Tip Videos",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Subscribe Ultra",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Watch History",
+      anonymous: "No",
+      lite: "Yes",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Feed Algorithm selection",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "No",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Likes",
+      anonymous: "No",
+      lite: "Yes(*)",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Post Comments",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Bookmarks",
+      anonymous: "No",
+      lite: "Yes",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+
+    {
+      byotube: "Maintain Lists",
+      anonymous: "No",
+      lite: "Yes",
+      liteFunded: "Yes",
+      plain: "Yes",
+      creator: "Yes",
+    },
+    {
+      byotube: "Video upload",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "No",
+      plain: "No",
+      creator: "Yes",
+    },
+    {
+      byotube: "Youtube Import",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "No",
+      plain: "Yes(*)",
+      creator: "Yes",
+    },
+    {
+      byotube: "Operate your own BYODA pod",
+      anonymous: "No",
+      lite: "No",
+      liteFunded: "No",
+      plain: "Yes",
+      creator: "Yes",
+    },
+  ];
+
+  const featuresTooltips = {
+    "Choose Nickname":
+      "Your nickname is unique and will show up on your channel and your comments",
+    "Choose Handle":
+      "Your handle is unique and people can go to your channel using www.byo.tube/channel/@<handle> (feature under development",
+    "Follow Creator": "Follow the creators of the content you love",
+    "Subscribe to Creator":
+      "You'll need to pay a monthly fee to see the subscriber-only content of the creator",
+    "Tip Videos": "Give the creator of the video a tip",
+    "Subscribe Ultra": "See videos hosted on BYO.Tube without ads",
+    "Watch History": "See a list of videos you have watched",
+    "Feed Algorithm selection":
+      "Select the algorithm that creates the 'For You' feed (feature under development)",
+    Likes: "Like/Dislike videos",
+    "Post Comments": "Post comments to videos (feature under development)",
+    Bookmarks: "Keep track of videos you have watched",
+    "Maintain Lists":
+      "Create your own lists and add videos to them (feature under development)",
+    "Video upload":
+      "Upload videos directly to BYO.Tube (feature under development)",
+    "Youtube Import": "Import your videos from YouTube",
+    "Operate your own BYODA pod":
+      "You can run the BYODA pod yourself or have it hosted at https://byo.host/",
+  };
+
+  const contentTooltips = {
+    "Follow Creator": "The creator does not know you follow them",
+    Likes: "Your likes are not visible to others",
+    "Youtube Import":
+      "Import of metadata only, videos will be streaming from and monetized by YouTube",
+  };
+
+  const liteFundedTooltip =
+    "To prevent bots and spam, you need to make a one-time payment of 2.99$ to upgrade from a 'Lite' account to 'Lite - funded' account";
+
   const getIconColor = (val) => {
     return val.includes("Yes") ? "green" : "red";
   };
@@ -25,9 +184,23 @@ export const useSignup = () => {
     return val.includes("Yes") ? val.replace("Yes", "") : val.replace("No", "");
   };
 
+  const getFeatureTooltip = (val) => {
+    return featuresTooltips[val];
+  };
+
+  const getContentTooltip = (val, item) => {
+    if (val.includes("*")) {
+      return contentTooltips[item?.byotube];
+    }
+  };
+
   return {
+    rows,
+    liteFundedTooltip,
     getIconColor,
     getIcon,
     getValue,
+    getFeatureTooltip,
+    getContentTooltip,
   };
 };
